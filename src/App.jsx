2d97369e8cd67abb18cd91986067b83f1fe59c94
Todor Daskalov <todorvdaskalov@gmail.com>
2024-01-10 20:36:58 +0200

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage";
 
 function App() {
     return (
         <>
-            <LoginPage />
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
         </>
     );
 }
