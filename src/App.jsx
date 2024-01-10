5e060df267d5066bd1b31ffea14b227761b804dd
Todor Daskalov <todorvdaskalov@gmail.com>
2024-01-10 22:03:56 +0200

    Add TablePage Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage";
+import TablePage from "./components/TablePage";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
+                <Route path="/table" element={<TablePage />} />
             </Routes>
         </>
     );
